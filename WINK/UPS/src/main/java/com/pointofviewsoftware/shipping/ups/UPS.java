0bf3ac7b77f8c7f00497d3b31067853afa9c1985
Christian Mokbel <cmokbel@msn.com>
2021-01-19 13:06:36 -0500

    fix unused imports

--- WINK/UPS/src/main/java/com/pointofviewsoftware/shipping/ups/UPS.java
+++ WINK/UPS/src/main/java/com/pointofviewsoftware/shipping/ups/UPS.java
@@ -5,32 +5,18 @@
  */
 package com.pointofviewsoftware.shipping.ups;
 
-import com.pointofviewsoftware.shipping.APICredentials;
-import com.pointofviewsoftware.shipping.Address;
-import com.pointofviewsoftware.shipping.IWinkShipper;
 import com.pointofviewsoftware.shipping.Package;
-import com.pointofviewsoftware.shipping.Rate;
+import com.pointofviewsoftware.shipping.*;
 import com.ups.wsdl.xoltws.rate.v1.RateErrorMessage;
 import com.ups.wsdl.xoltws.rate.v1.RatePortType;
 import com.ups.wsdl.xoltws.rate.v1.RateService;
 import com.ups.xmlschema.xoltws.common.v1.RequestType;
-import com.ups.xmlschema.xoltws.rate.v1.AddressType;
-import com.ups.xmlschema.xoltws.rate.v1.CodeDescriptionType;
-import com.ups.xmlschema.xoltws.rate.v1.DimensionsType;
-import com.ups.xmlschema.xoltws.rate.v1.PackageType;
-import com.ups.xmlschema.xoltws.rate.v1.PackageWeightType;
-import com.ups.xmlschema.xoltws.rate.v1.RateRequest;
-import com.ups.xmlschema.xoltws.rate.v1.RateResponse;
-import com.ups.xmlschema.xoltws.rate.v1.RatedShipmentType;
-import com.ups.xmlschema.xoltws.rate.v1.ShipFromType;
-import com.ups.xmlschema.xoltws.rate.v1.ShipToAddressType;
-import com.ups.xmlschema.xoltws.rate.v1.ShipToType;
-import com.ups.xmlschema.xoltws.rate.v1.ShipmentType;
-import com.ups.xmlschema.xoltws.rate.v1.ShipperType;
+import com.ups.xmlschema.xoltws.rate.v1.*;
 import com.ups.xmlschema.xoltws.upss.v1.UPSSecurity;
+
+import javax.xml.ws.BindingProvider;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.ws.BindingProvider;
 
 /**
  *
